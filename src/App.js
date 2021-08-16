c38fb5c35228cef7f38dd8d2980af63034dbe26a
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-16 15:41:57 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import NotFoundPage from './pages/404';
 
 export default function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </Router>
   );
 }
