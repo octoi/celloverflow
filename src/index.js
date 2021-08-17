a0fa6da879400ae55b8748f21e4098222fe9e163
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-17 11:08:28 +0530

    basic styling

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import ChakraWrap from './styles/chakraWrapper';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
+import './App.css';
+
 ReactDOM.render(
   <Provider store={store}>
-    <ChakraWrap>
-      <App />
-    </ChakraWrap>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
