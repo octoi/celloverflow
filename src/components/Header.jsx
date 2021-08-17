1cf5056585aa5041b88a5aab261e974327e874d1
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-17 11:50:17 +0530

    logos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { HeaderContainer } from '../styles/headerStyles';
+
 import LogoText from '../assets/logo_text.svg';
+import LogoIcon from '../assets/logo.svg';
 
 export default function Header() {
   return (
     <HeaderContainer>
-      <img src={LogoText} alt="" />
+      <div>
+        <img className='logo-text' src={LogoText} alt='celloverflow' />
+        <img className='logo-icon' src={LogoIcon} alt='celloverflow' />
+      </div>
     </HeaderContainer>
   )
 }
--- src/styles/headerStyles.js
+++ src/styles/headerStyles.js
@@ -1,5 +1,20 @@
 import styled from 'styled-components';
 
-export const HeaderContainer = styled.div`
+export const HeaderContainer = styled.div `
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
+  /* hiding & displaying logos accordingly  */
+  img { width: 200px; }
+
+  .logo-icon { display: none; }  
+
+  @media (max-width: 800px) {
+
+    .logo-icon { display: block; }
+    .logo-text { display: none; }
+    img { width: 40px; }
+    
+  }
 `
