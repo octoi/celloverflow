17c0d860e14c1405bd6c36eb35122fd5b7726950
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-25 13:49:51 +0530

    Never gonna say goodbye

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,23 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import { useParams } from 'react-router-dom';
 import { useToast } from '@chakra-ui/react';
 import { getQuestionById } from '../firebase/helpers/questionHelper';
-import { Container, QuestionAndAnswerWrapper, ParticipantsWrapper } from '../styles/questionStyles';
+import { Container, QuestionAndAnswerWrapper } from '../styles/questionStyles';
 import AuthWrapper from '../components/auth/AuthWrapper';
-import Participants from '../components/question/Participants';
 import QuestionPart from '../components/question/QuestionPart';
 import Answers from '../components/question/Answers';
 
 export default function Question() {
   const [question, setQuestion] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   const { questionId } = useParams();
 
   const showToast = useToast();
 
   useEffect(() => {
+    setIsLoading(true);
     getQuestionById(questionId).then(questionFromFirestore => {
       setQuestion(questionFromFirestore);
+      setIsLoading(false);
     }).catch(err => {
+      setIsLoading(false);
       showToast({
         title: 'Failed to fetch data 😭',
         duration: 3000,
@@ -31,13 +35,17 @@ export default function Question() {
   return (
     <AuthWrapper authenticationRequired={true}>
       <Container>
-        <QuestionAndAnswerWrapper>
-          {question && <QuestionPart question={question} />}
+        {isLoading && (
+          <div style={{ width: "100%" }}>
+            <SkeletonTheme color="var(--secondary-color)" highlightColor="#444">
+              <Skeleton height={500} count={3} style={{ borderRadius: '25px', marginBottom: "10px" }} />
+            </SkeletonTheme>
+          </div>
+        )}
+        {!isLoading && question && <QuestionAndAnswerWrapper>
+          <QuestionPart question={question} />
           <Answers />
-        </QuestionAndAnswerWrapper>
-        <ParticipantsWrapper>
-          <Participants />
-        </ParticipantsWrapper>
+        </QuestionAndAnswerWrapper>}
       </Container>
     </AuthWrapper>
   )
--- src/styles/questionStyles.js
+++ src/styles/questionStyles.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   margin-top: 30px;
+  width: 100%;
+  height: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -9,58 +11,9 @@ export const Container = styled.div`
 
 export const QuestionAndAnswerWrapper = styled.div`
   width: 100%;
-  margin-right: 20px;
 
   @media (max-width: 1446px) {
     margin-right: 0px;
-  }
-`
-
-export const ParticipantsWrapper = styled.div`
-  width: 480px !important;
-  background: var(--secondary-color);
-  border-radius: 25px;
-  padding: 40px;
-  height: fit-content;
-
-  @media (max-width: 1446px) {
-    display: none;
-  }
-
-  h2 {
-    font-size: 25px;
-    font-weight: 600;
-    margin-bottom: 20px;
-  }
-
-  .profile {
-    display: flex;
-    align-items: center;
-    margin-top: 20px;
-    cursor: pointer;
-
-    img {
-      width: 70px;
-      height: 70px;
-      border-radius: 50%;
-      border: 3px solid var(--accent-color);
-      object-fit: cover;
-    }
-    div {
-      display: flex;
-      flex-direction: column;
-      margin-left: 10px;
-    }
-
-    h3 {
-      font-size: 22px;
-      font-weight: 600;
-    }
-
-    h4 {
-      color: var(--text-opacity-color);
-      font-size: 18px;
-    }
   }
 `
 
