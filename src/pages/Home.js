aa6c33c764a7e4d0d5e7538c6406bb9cc84e0b74
fadhilsaheer <fadhilsaheer@gmail.com>
2021-08-27 19:59:25 +0530

    Never gonna run around and desert you

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import AuthWrapper from '../components/auth/AuthWrapper';
+import { Container } from '../styles/homeStyles';
 
 export default function Home() {
   return (
     <AuthWrapper authenticationRequired={false}>
-
+      <Container>
+        Hello World is awesome
+      </Container>
     </AuthWrapper>
   )
 }
