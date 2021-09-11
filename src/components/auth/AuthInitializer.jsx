36c1e7dd3efb6a1747b26c71e9ddd8756b983acb
fadhilsaheer <fadhilsaheer@gmail.com>
2021-09-11 18:58:50 +0530

    added loading for auth state

--- src/components/auth/AuthInitializer.jsx
+++ src/components/auth/AuthInitializer.jsx
@@ -1,30 +1,46 @@
 // Get user data from session if exists
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import getUserObject from '../../utils/loginHelper';
 import { auth } from '../../firebase/firebase';
 import { getUserData } from '../../firebase/helpers/userHelper';
 import { useDispatch } from 'react-redux';
 import { Login as userLogin } from '../../redux/actions/userLogin';
+import { Flex } from '@chakra-ui/react';
 
 export default function AuthInitializer({ children }) {
+  const [isUserStateLoading, setIsUserStateLoading] = useState(false);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
+    setIsUserStateLoading(true);
+
     auth.onAuthStateChanged(userData => {
-      if (!userData) return;
+      if (!userData) {
+        setIsUserStateLoading(false);
+        return;
+      };
+
       const user = getUserObject(userData);
+
       getUserData(user?.email).then(userData => {
         dispatch(userLogin(userData));
+        setIsUserStateLoading(false);
       }).catch(() => {
         dispatch(userLogin(user));
+        setIsUserStateLoading(false);
       });
     })
   }, [dispatch]);
 
   return (
-    <div>
-      {children}
-    </div>
+    isUserStateLoading
+      ? <Flex height="95vh" justifyContent="center" alignItems="center">
+        <img src="https://c.tenor.com/ULJn9_2K17UAAAAi/wazatoz-rick-roll.gif" alt="Loading" />
+      </Flex>
+      : <div>
+        {children}
+      </div>
   )
 }
